c3d40b7028c57a7bed93da7d682a527270d382e1
MarijaMidas <marija.midas@gmail.com>
2022-12-13 15:58:24 +0300

    Created Sort class to HomeWork

---
+++ src/JavaSyntax/SortBubble/Sort.java
@@ -0,0 +1,34 @@
+package JavaSyntax.SortBubble;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+
+public class Sort {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] nums = new int[1000];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = random.nextInt(1000);
+        }
+        System.out.println(new Date().getTime());
+        bubbleSort(nums);
+        System.out.println(new Date().getTime());
+        System.out.println("-------------------");
+        System.out.println(new Date().getTime());
+        Arrays.sort(nums);
+        System.out.println(new Date().getTime());
+    }
+    public static int[] bubbleSort(int[]num) {
+        for (int i = num.length - 1; i > 0; i--) {
+            for (int j = 0; j < i ; j++) {
+                if(num[j]>num[j+1]){
+                    int cashNum = num[j+1];
+                    num[j+1] = num[j];
+                    num[j] = cashNum;
+                }
+            }
+        }
+        return num;
+    }
+}
